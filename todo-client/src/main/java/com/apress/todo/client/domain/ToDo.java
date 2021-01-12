5d39d15007a702c60657995f10a0634bbb3952d4
zzz <zzz@192.168.2.106>
2021-01-12 16:00:58 +0100

    Domain

---
+++ todo-client/src/main/java/com/apress/todo/client/domain/ToDo.java
@@ -0,0 +1,28 @@
+package com.apress.todo.client.domain;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import lombok.Data;
+
+@Data
+public class ToDo {
+
+	private String id;
+	private String description;
+	private LocalDateTime created;
+	private LocalDateTime modified;
+	private boolean completed;
+
+	private ToDo() {
+		LocalDateTime date= LocalDateTime.now();
+		this.id= UUID.randomUUID().toString();
+		this.created= date;
+		this.modified= date;
+	}
+	
+	public ToDo(String description) {
+		this();
+		this.description= description;
+	}
+}
